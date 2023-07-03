c1e9ea3bf8feeaaee9a7bbc828fd9df8b0fca567
cuongdang <dangxuancuong354@gmail.com>
2023-07-03 19:33:28 +0700

    react component props

--- src/App.tsx
+++ src/App.tsx
@@ -3,68 +3,59 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
 const data = [
   {
-    category: 'Sporting goods',
-    items: [
-      {
-        name: 'football',
-        price: 23
-      },
-      {
-        name: 'baseball',
-        price: 28
-      },
-      {
-        name: 'basketball',
-        price: 30
-      }
-    ]
+    img: 'https://i1-kinhdoanh.vnecdn.net/2023/07/03/chungkhoan2-1688376716-6266-1688376742.jpg?w=680&h=408&q=100&dpr=2&fit=crop&s=C8bfKsfxytPkncS8guvEoQ',
+    title: 'Tiền vào bắt đáy các cổ phiếu \'họ APEC\'',
+    content: 'Các cổ phiếu "họ APEC" trở thành tâm điểm phiên đầu tuần khi dòng tiền bắt đáy tăng mạnh, với khối lượng khớp lệnh 7-10 triệu cổ phiếu.',
+    category: 'Chứng khoán'
   },
   {
-    category: 'Electronics',
-    items: [
-      {
-        name: 'ipod',
-        price: 500
-      },
-      {
-        name: 'iphone',
-        price: 5000
-      },
-      {
-        name: 'nexus',
-        price: 2000
-      }
-    ]
+    title: 'Giá xăng giảm gần 600 đồng một lít',
+    content: 'Xăng RON 95-III giảm 590 đồng, E5 RON 92 hạ 400 đồng, các mặt hàng dầu (trừ dầu mazut) cũng hạ 10-30 đồng một lít, từ 15h chiều nay.',
+    category: 'Vĩ mô'
+  },
+  {
+    title: 'Trung Quốc gom khí đốt toàn cầu',
+    content: 'Trung Quốc ký nhiều hợp đồng mua khí hóa lỏng (LNG) để đảm bảo nguồn cung trong nước hoặc bán cho nước khác khi nhu cầu nội địa yếu.',
+    category: 'Quốc tế'
+  },
+  {
+    title: ' \'Đầu tàu\' TP HCM bắt đầu chuyển động\n',
+    content: 'Chưa như kỳ vọng nhưng tình hình đang sáng dần lên, ông Nguyễn Ngọc Thắng, Giám đốc Khối Vận hành chuỗi siêu thị Co.opmart, mô tả hoạt động nửa đầu năm.',
+    category: 'Vĩ mô'
   }
-];
+]
 
-function Product({name, price}: {name: string, price: number}) {
-  return <div>
-    <p>{name} {price}</p>
+const Article = ({title, category, content}: {title: string, content: string, category: string}) => {
+  return <div style={{ width: '30%', marginRight: 10}}>
+    <p className="title">{title}</p>
+    <p className="content">{content}</p>
+    <p style={{color: 'black'}}>{category}</p>
   </div>
 }
 
-function Category({name, listItem}: {name: string, listItem: Array<any>}) {
-  console.log(name)
-  console.log(listItem)
-  return <div>
-    <div style={{ border: '1px solid blue', width: 200, marginTop: 20}}>
-      <p>{name}</p>
-      {
-        listItem.map((item, index) => <Product  name={item.name} price={item.price} key={index}/>)
-      }
-    </div>
-  </div>
-}
 function App() {
   return (
-    <div>
-      {
-        data.map((cate, index) => <Category  key={index}  listItem={cate.items} name={cate.category} />)
-      }
+    <div className='container'>
+      <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', width: 1000}}>
+        {
+          data.map((item, index) => {
+            if (index === 0) {
+              return <div style={{ width: 1000, display: 'flex', flexDirection: 'row', backgroundColor: '#f7f7f7' }}>
+                <img src={item.img} style={{ width: 700, height: 'auto' }}/>
+                <div style={{padding: 20}}>
+                  <p className="title">{item.title}</p>
+                  <p className="content">{item.content}</p>
+                </div>
+              </div>
+            } else {
+              return <Article  category={item.category} content={item.content} title={item.title} key={index} />
+            }
+          })
+        }
+      </div>
+
     </div>
   )
 }
